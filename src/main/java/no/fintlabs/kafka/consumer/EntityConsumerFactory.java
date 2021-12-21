fd0c2fe540afaa6b9a5c3625a69242bc52d083d6
Eivind Morch <eivind@morch.priv.no>
2021-12-21 10:25:47 +0100

    FST-92 #comment Make entity consumer seek aware

--- src/main/java/no/fintlabs/kafka/consumer/EntityConsumerFactory.java
+++ src/main/java/no/fintlabs/kafka/consumer/EntityConsumerFactory.java
@@ -2,10 +2,13 @@ package no.fintlabs.kafka.consumer;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 import no.fintlabs.kafka.consumer.cache.FintCache;
 import no.fintlabs.kafka.consumer.cache.FintCacheManager;
 import no.fintlabs.kafka.topic.DomainContext;
 import no.fintlabs.kafka.topic.TopicNameService;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.TopicPartition;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -14,13 +17,13 @@ import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.listener.MessageListener;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
+import java.util.Map;
 import java.util.function.Function;
 
+@Slf4j
 @Service
-public class EntityConsumerFactory extends AbstractConsumerSeekAware {
+public class EntityConsumerFactory {
 
     @Value(value = "${spring.kafka.consumer.group-id}")
     private String consumerGroupId;
@@ -41,6 +44,10 @@ public class EntityConsumerFactory extends AbstractConsumerSeekAware {
         this.topicNameService = topicNameService;
     }
 
+    public static abstract class EntityConsumer extends AbstractConsumerSeekAware implements MessageListener<String, String> {
+
+    }
+
     public <R> ConcurrentMessageListenerContainer<String, String> createEntityConsumer(
             DomainContext domainContext,
             String resourceReference,
@@ -48,48 +55,36 @@ public class EntityConsumerFactory extends AbstractConsumerSeekAware {
             Function<R, List<String>> keyMapper,
             boolean resetOffsetOnCreation
     ) {
-        ConcurrentMessageListenerContainer<String, String> entityConsumer = this.createEntityConsumerContainer(
-                domainContext,
-                resourceReference,
-                resourceClass,
-                keyMapper
-        );
-        entityConsumer.start();
-        if (resetOffsetOnCreation) {
-            Optional.ofNullable(entityConsumer.getAssignedPartitions()).ifPresent(this::seekOffsetsToBeginning);
-        }
-        return entityConsumer;
-    }
-
-    private <R> ConcurrentMessageListenerContainer<String, String> createEntityConsumerContainer(
-            DomainContext domainContext,
-            String resourceReference,
-            Class<R> resourceClass,
-            Function<R, List<String>> keyMapper
-    ) {
         String topic = this.topicNameService.generateEntityTopicName(domainContext, resourceReference);
         FintCache<String, R> cache = this.fintCacheManager.createCache(resourceReference, String.class, resourceClass);
         ConcurrentMessageListenerContainer<String, String> container = kafkaListenerContainerFactory.createContainer(topic);
         container.getContainerProperties().setGroupId(this.consumerGroupId);
-        container.getContainerProperties().setMessageListener((MessageListener<String, String>) consumerRecord -> {
-            try {
-                R resource = objectMapper.readValue(consumerRecord.value(), resourceClass);
-                List<String> keys = keyMapper.apply(resource);
-                keys.forEach(key -> cache.put(key, resource));
-            } catch (JsonProcessingException e) {
-                e.printStackTrace();
+
+        EntityConsumer entityConsumer = new EntityConsumer() {
+
+            @Override
+            public void onMessage(ConsumerRecord<String, String> consumerRecord) {
+                try {
+                    R resource = objectMapper.readValue(consumerRecord.value(), resourceClass);
+                    List<String> keys = keyMapper.apply(resource);
+                    keys.forEach(key -> cache.put(key, resource));
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
             }
-        });
+
+            @Override
+            public void onPartitionsAssigned(@NonNull Map<TopicPartition, Long> assignments, @NonNull ConsumerSeekCallback callback) {
+                super.onPartitionsAssigned(assignments, callback);
+                if (resetOffsetOnCreation) {
+                    callback.seekToBeginning(assignments.keySet());
+                }
+            }
+        };
+
+        container.getContainerProperties().setMessageListener(entityConsumer);
+        container.start();
         return container;
-    }
-
-    private void seekOffsetsToBeginning(Collection<TopicPartition> partitions) {
-        partitions.forEach(this::seekOffsetToBeginning);
-    }
-
-    private void seekOffsetToBeginning(TopicPartition partition) {
-        Optional.ofNullable(getSeekCallbackFor(partition))
-                .ifPresent(callback -> callback.seekToBeginning(partition.topic(), partition.partition()));
     }
 
 }
