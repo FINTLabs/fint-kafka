50214d3ffbc4bcce5ad6be8fa6baac4a415291ea
Frode Sjovatsen <frodesjovatsen@Frodes-MacBook-Pro.local>
2022-02-17 21:44:35 +0100

    FC-71 #comment Removed caching code #in-progress

--- src/main/java/no/fintlabs/kafka/consumer/cache/FintCacheEventListener.java
+++
@@ -1,7 +0,0 @@
-package no.fintlabs.kafka.consumer.cache;
-
-public interface FintCacheEventListener<K, V> {
-
-    void onEvent(FintCacheEvent<K, V> event);
-
-}
--- src/main/java/no/fintlabs/kafka/consumer/cache/exceptions/NoSuchCacheException.java
+++
@@ -1,9 +0,0 @@
-package no.fintlabs.kafka.consumer.cache.exceptions;
-
-public class NoSuchCacheException extends RuntimeException {
-
-    public NoSuchCacheException(String alias) {
-        super(String.format("No cache with alias='%s'", alias));
-    }
-
-}
