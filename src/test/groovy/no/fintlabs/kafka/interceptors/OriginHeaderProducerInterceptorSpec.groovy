80763b8d4d9b0750a812bd4693885b861d18882c
Frode Sjovatsen <frodesjovatsen@Frodes-MacBook-Pro.local>
2022-02-27 23:17:31 +0100

    Fixed tests

--- src/test/groovy/no/fintlabs/kafka/interceptors/OriginHeaderProducerInterceptorSpec.groovy
+++ src/test/groovy/no/fintlabs/kafka/interceptors/OriginHeaderProducerInterceptorSpec.groovy
@@ -1,75 +1,46 @@
 package no.fintlabs.kafka.interceptors
 
-
-import no.fintlabs.kafka.KafkaTestListener
+import no.fintlabs.kafka.KafkaConsumer
 import no.fintlabs.kafka.OriginHeaderProducerInterceptor
+
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.context.TestConfiguration
-import org.springframework.context.annotation.Bean
-import org.springframework.kafka.core.ConsumerFactory
 import org.springframework.kafka.core.KafkaTemplate
-import org.springframework.kafka.listener.ConcurrentMessageListenerContainer
-import org.springframework.kafka.listener.ContainerProperties
 import org.springframework.kafka.test.context.EmbeddedKafka
-import org.springframework.test.annotation.DirtiesContext
-import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.TestPropertySource
 import spock.lang.Specification
 
 import java.nio.charset.StandardCharsets
+import java.util.concurrent.TimeUnit
 
-@SpringBootTest(properties = ["spring.main.allow-bean-definition-overriding=true"])
-@ContextConfiguration(classes = Configuration.class)
-@TestPropertySource(properties = ["spring.config.location = classpath:application-test.yaml"])
-@EmbeddedKafka(brokerProperties = ['listeners=PLAINTEXT:${spring.kafka.bootstrap-servers}'], partitions = 1)
-@DirtiesContext
+@SpringBootTest
+@EmbeddedKafka
 class OriginHeaderProducerInterceptorSpec extends Specification {
 
     @Autowired
     KafkaTemplate<String, String> kafkaTemplate
 
     @Autowired
-    KafkaTestListener kafkaTestListener
+    KafkaConsumer kafkaConsumer
 
-    @TestConfiguration
-    static class Configuration {
-
-        @Bean
-        KafkaTestListener testListener() {
-            return new KafkaTestListener();
-        }
-
-        @Bean
-        ConcurrentMessageListenerContainer<String, String> testListenerContainer(
-                ConsumerFactory<String, String> consumerFactory,
-                KafkaTestListener testListener
-        ) {
-            ContainerProperties containerProperties = new ContainerProperties("test-topic")
-            ConcurrentMessageListenerContainer<String, String> container =
-                    new ConcurrentMessageListenerContainer<>(consumerFactory, containerProperties)
-            container.getContainerProperties().setGroupId("testConsumerGroup")
-            container.getContainerProperties().setMessageListener(testListener)
-            container.start()
-            return container
-
-        }
-    }
 
     def 'kafka template should add origin application id header to producer record'() {
         given:
-        kafkaTestListener.trackNextRecords(1);
-        when:
         kafkaTemplate.send("test-topic", "test-message")
-        kafkaTestListener.waitForRecords()
+
+        when:
+        kafkaConsumer.getLatch().await(2, TimeUnit.SECONDS);
+
         then:
-        kafkaTestListener.consumedRecords.size() == 1
-        def consumedRecord = kafkaTestListener.consumedRecords.get(0)
-        consumedRecord.topic() == "test-topic"
-        consumedRecord.value() == "test-message"
-        consumedRecord.headers().headers(OriginHeaderProducerInterceptor.ORIGIN_APPLICATION_ID_PRODUCER_CONFIG).size() == 1
-        consumedRecord.headers().lastHeader(OriginHeaderProducerInterceptor.ORIGIN_APPLICATION_ID_PRODUCER_CONFIG).value()
-                == "test.application.id".getBytes(StandardCharsets.UTF_8)
+        kafkaConsumer.getLatch().getCount() == 0L
+        kafkaConsumer.getPayload().value() == "test-message"
+        kafkaConsumer.getPayload()
+                .headers()
+                .headers(OriginHeaderProducerInterceptor.ORIGIN_APPLICATION_ID_PRODUCER_CONFIG)
+                .size() == 1
+        new String(kafkaConsumer.getPayload()
+                .headers()
+                .lastHeader(OriginHeaderProducerInterceptor.ORIGIN_APPLICATION_ID_PRODUCER_CONFIG)
+                .value(), StandardCharsets.UTF_8) == "test"
     }
 
 }
