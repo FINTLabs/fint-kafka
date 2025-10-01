132aace6a7502ecb04aa49b8162a3e94fec4335d
Jan Ove Kongshaug <jan.ove.kongshaug@novari.no>
2025-10-01 08:34:07 +0200

    Change EntityConsumerConfiguration to allow changing seekingOffsetResetOnAssignment for v3.2.0-rc-2

--- src/main/java/no/fintlabs/kafka/entity/EntityConsumerConfiguration.java
+++ src/main/java/no/fintlabs/kafka/entity/EntityConsumerConfiguration.java
@@ -12,15 +12,11 @@ public class EntityConsumerConfiguration implements ListenerConfiguration {
 
     private final String groupIdSuffix;
     private final CommonErrorHandler errorHandler;
+    @Builder.Default private final boolean seekingOffsetResetOnAssignment = true;
     private final OffsetSeekingTrigger offsetSeekingTrigger;
 
     public static EntityConsumerConfiguration empty() {
         return EntityConsumerConfiguration.builder().build();
-    }
-
-    @Override
-    public boolean isSeekingOffsetResetOnAssignment() {
-        return true;
     }
 
 }
